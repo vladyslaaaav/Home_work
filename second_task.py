8f8077c228790369726f8ff46053ff25c84d7442
Grygorash Vladyslav <103047580+vladyslaaaav@users.noreply.github.com>
2022-04-05 16:58:49 +0300

    Add files via upload

---
+++ second_task.py
@@ -0,0 +1,16 @@
+#Задача: Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+
+my_bool = True
+my_bool = False
+
+result = 10 < 30
+print(result)
+
+result = 10 > 30
+print(result)
+
+result = 10 == 30
+print(result)
+
+result = 10 != 30
+print(result)
